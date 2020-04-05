15371be603f807b2b5c35732c0c1a3cde54d55de
abdallahshanaah <abdallah_m_sh@yahoo.com>
2020-04-06 02:06:11 +0300

    add file and data

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+
+//CORS = Cross Origin Resource Sharing
+const cors = require('cors');
+
+// DOTENV (Read our Enviroment Variable)
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const server = express();
